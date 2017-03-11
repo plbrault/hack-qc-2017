383f94d473b64ad60d6d953daa96f1f21d128377
Steve Bourgoin <stiv.aw@gmail.com>
2017-03-11 13:37:47 -0500

    add address inputs

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import { AppRegistry } from 'react-native';
 import { StackNavigator } from 'react-navigation';
 
-import Home from './Home';
-import Search from './Search';
+import Home from './components/Home';
+import Search from './components/Search/Search';
 
 const App = StackNavigator({
-  Home: { screen: Home },
   Search: { screen: Search },
+  Home: { screen: Home },
 });
 
 AppRegistry.registerComponent('HackQc17', () => App);
