e15a285e62b91296a5c9ebc9d166e6fc8f5b0d21
Pier-Luc Brault <plbrault@gmail.com>
2017-03-11 14:17:44 -0500

    Fetching distances

--- src/components/DataView.js
+++ src/components/DataView.js
@@ -7,13 +7,26 @@ import {
 import parkingData from '../lib/parkingData';
 import { sortByProximity } from '../lib/parkingSorter';
 
-const parkings = sortByProximity(parkingData, 45.421804, -71.963097);
-
 export default class DataView extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { parkings: [] };
+  }
+
+  componentDidMount() {
+    this.fetchData();
+  }
+
+  async fetchData() {
+    const parkings = await sortByProximity(parkingData, 45.421804, -71.963097);
+    this.setState({ parkings });
+    alert(parkings);
+  }
+
   render() {
     return (
       <ScrollView>
-        { parkings.map(parking => <Text key={parking.name}>{JSON.stringify(parking)}</Text>) }
+        { this.state.parkings.map(parking => <Text key={parking.name}>{JSON.stringify(parking)}</Text>) }
       </ScrollView>
     );
   }
--- src/lib/parkingSorter.js
+++ src/lib/parkingSorter.js
@@ -1,16 +1,47 @@
-import gju from 'geojson-utils';
+import settings from '../settings.json';
+
+async function getDistances(parkingData, currentLat, currentLon) {
+  const origin = `${currentLat},${currentLon}`;
+  const destinations = parkingData.reduce((dest, parking) => `${dest}${parking.lat},${parking.lon}|`, '');
+  const url = `https://maps.googleapis.com/maps/api/distancematrix/json?units=metric&origins=${origin}&destinations=${destinations}&key=${settings.googleApiKey}`;
+
+  return new Promise((resolve, reject) => {
+    fetch(url)
+      .then(response => response.json())
+      .then(responseJson => responseJson.rows)
+      .then(rows => resolve(rows))
+      .catch(err => reject(err));
+  });
+}
 
 /**
  * Return an array of parkings, sorted from the nearest to the farthest,
- * with an added `distance` attribute.
+ * with an added `distanceInfo` attribute.
+ *
+ * `distanceInfo` example:
+ * {
+ *  index: null,
+ *  distance: '807 km',
+ *  distanceValue: 807366,
+ *  duration: '7 hours 30 mins',
+ *  durationValue: 26981,
+ *  origin: 'San Francisco, CA, USA',
+ *  destination: 'San Diego, CA, USA',
+ *  mode: 'driving',
+ *  units: 'metric',
+ *  language: 'en',
+ *  avoid: null,
+ *  sensor: false
+ *}
  */
-export function sortByProximity(parkingData, currentLat, currentLon) {
-  const currentPoint = { type: 'Point', coordinates: [currentLon, currentLat] };
-  return parkingData.map(parking => ({
+export async function sortByProximity(parkingData, currentLat, currentLon) {
+  const distances = await getDistances(parkingData, currentLat, currentLon);
+  alert(distances);
+  return parkingData.map((parking, idx) => ({
     ...parking,
-    distance: gju.pointDistance(currentPoint, parking.geojson),
-  }))
-  .sort((p1, p2) => p1.distance < p2.distance ? -1 : 1);
+    distanceInfo: distances[idx],
+  }));
+  //.sort((p1, p2) => (p1.distanceInfo.durationValue < p2.distanceInfo.durationValue ? -1 : 1));*/
 }
 
 export default {
