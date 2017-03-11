c7ee577661f24778cebab570f60505cb05b63889
Pier-Luc Brault <plbrault@gmail.com>
2017-03-11 17:01:05 -0500

    WIP

--- src/lib/parkingSorter.js
+++ src/lib/parkingSorter.js
@@ -17,16 +17,25 @@ async function getDistancesToParkings(parkingData, originLat, originLon, departu
     .then(responseJson => responseJson.rows[0].elements);
 }
 
+async function getDistancesToDestination(parkingData, destinationLat, destinationLon, departureTime, arrivalTime) {
+  const origins = parkingData.reduce((dest, parking) => `${dest}${parking.lat},${parking.lon}|`, '');
+  const destination = `${destinationLat},${destinationLon}`;
+
+  let url;
+  if (departureTime) {
+    url = `https://maps.googleapis.com/maps/api/distancematrix/json?units=metric&origins=${origins}&destinations=${destination}&mode=transit&departure_time=${departureTime}&key=${settings.googleApiKey}`;
+  } else {
+    url = `https://maps.googleapis.com/maps/api/distancematrix/json?units=metric&origins=${origins}&destinations=${destination}&mode=transit&arrival_time=${arrivalTime}&key=${settings.googleApiKey}`;
+  }
+
+  return fetch(url) // eslint-disable-line no-undef
+    .then(response => response.json())
+    .then(responseJson => responseJson.rows[0].elements);
+}
+
 /**
  * Return an array of parkings, sorted from the shortest to longest total duration,
- * with an added `distanceInfo` attribute.
- *
- * `distanceInfo` example:
- * {
- *  distance: { text: '136 km', value: 135899 },
- *  duration: { text: '1 hour 22 mins , value: 144290 },
- *  status: 'OK',
- * }
+ * with an added `totalDuration` (in seconds) attribute.
  *
  * @param [object[]] parkingData
  * @param [number] currentLat
@@ -40,9 +49,10 @@ export async function sortByProximity(
   parkingData, originLat, originLon, destinationLat, destinationLon, departureTime, arrivalTime,
 ) {
   const distancesToParkings = await getDistancesToParkings(parkingData, originLat, originLon, departureTime);
+  const distancesToDestination = await getDistancesToDestination(parkingData, destinationLat, destinationLon, departureTime, arrivalTime);
   return parkingData.map((parking, idx) => ({
     ...parking,
-    distanceInfo: distancesToParkings[idx],
+    totalDuration: distancesToParkings[idx].duration.value + distancesToDestination[idx].value + (15 * 60),
   }))
   .sort((p1, p2) => (p1.distanceInfo.duration.value < p2.distanceInfo.duration.value ? -1 : 1));
 }
