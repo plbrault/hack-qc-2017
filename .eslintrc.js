7afd2841382f5460e4bafd73d700c161726f85a2
Steve Bourgoin <stiv.aw@gmail.com>
2017-03-10 21:37:41 -0500

    add eslint

---
+++ .eslintrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+  parser: "babel-eslint",
+  extends: "airbnb",
+  plugins: [
+    "react",
+    "react-native"
+  ],
+  rules: {
+    "react/jsx-filename-extension": [1, { "extensions": [".js", ".jsx"] }],
+  },
+};
