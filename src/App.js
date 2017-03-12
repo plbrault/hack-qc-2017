f026400ef51fd7039469d6b020b0a9f348a5e81b
Pier-Luc Brault <plbrault@gmail.com>
2017-03-11 23:44:06 -0500

    Remove Home

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
 import { AppRegistry } from 'react-native';
 import { StackNavigator } from 'react-navigation';
 
-import Home from './components/Home';
 import Results from './components/Results/Results';
 import DataView from './components/DataView';
 import Search from './components/Search/Search';
 import Navigation from './components/Navigation';
 
 const App = StackNavigator({
-  Home: { screen: Home },
   Navigation: { screen: Navigation },
   DataView: { screen: DataView },
   Search: { screen: Search },
